9fc1586d85ebd349e49d4f73da801b6c3a1db932
Natã Muniz <natan.m25@gmail.com>
2023-02-05 22:11:18 -0300

    updatings

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
-server.get("/curso", () => {});
+server.get("/curso:id", () => {
+  const id = req.query.id;
+
+  return resizeBy.json({ curso: `Curso: ${id}` });
+});
 
 server.listen(3000);
