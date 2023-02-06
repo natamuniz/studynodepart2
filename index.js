2e925e433ffbfbc197fc7d075b5ffb90284b6e0c
Natã Muniz <natan.m25@gmail.com>
2023-02-06 07:45:00 -0300

    Adding CRUD

--- index.js
+++ index.js
@@ -2,10 +2,39 @@ const express = require("express");
 
 const server = express();
 
-server.get("/curso:id", () => {
-  const id = req.query.id;
+server.use(express.json());
 
-  return resizeBy.json({ curso: `Curso: ${id}` });
+const cursos = ["Node JS", "JavaScript", "React Native"];
+
+server.use((req, res, next) => {
+  console.log("Requisição chamada");
+
+  return next();
+});
+
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
+});
+
+server.get("/cursos/:index", () => {
+  const { index } = req.params;
+
+  return resizeBy.json(cursos[index]);
+});
+
+server.post("/cursos", (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+server.put("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+  return res.json(cursos);
 });
 
 server.listen(3000);
